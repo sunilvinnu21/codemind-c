eb4c7dace59fd9b5472acd9bf62434d2695dbf07
sunilvinnu21 <100119381+sunilvinnu21@users.noreply.github.com>
2022-07-07 15:31:55 +0530

    #include<stdio.h> int main() {     int a,b,gcd,count=1;     scanf("%d%d",&a,&b);     while(count<=a && count<=b)     {         if(a%count==0 && b%count==0)         {             gcd=count;         }         count++;     }     printf("%d ",gcd);     return 0; }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,gcd,count=1;
+    scanf("%d%d",&a,&b);
+    while(count<=a && count<=b)
+    {
+        if(a%count==0 && b%count==0)
+        {
+            gcd=count;
+        }
+        count++;
+    }
+    printf("%d
+",gcd);
+    return 0;
+}
